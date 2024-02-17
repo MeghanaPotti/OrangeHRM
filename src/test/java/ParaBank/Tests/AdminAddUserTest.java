3c670e948004cf8f17e4ef28284c84de6ccfa931
pottimeghana123@gmail.com <pottimeghana123@gmail.com>
2024-02-17 21:59:32 +0530

    script

--- src/test/java/ParaBank/Tests/AdminAddUserTest.java
+++ src/test/java/ParaBank/Tests/AdminAddUserTest.java
@@ -11,6 +11,7 @@ public class AdminAddUserTest extends LoginTest{
 	@Test
 	public void adminAddUserTest() {
 		System.out.println("This is an admin add user method");
+		System.out.println("This is an admin add user method");
 		AdminAddUserPage newUserPage = new AdminAddUserPage(driver);
 		newUserPage.login("Admin", "admin123", driver);
 		newUserPage.adminAddNewUser("Admin", "@Admin123");
